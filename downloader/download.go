57d946b204f78c6a79f728612c25adcfdf945d57
Alexander <abonec@gmail.com>
2018-08-18 16:57:34 +0300

    parallel download

--- downloader/download.go
+++ downloader/download.go
@@ -1,21 +1,47 @@
 package downloader
 
 import (
-	"net/http"
 	"github.com/abonec/file_downloader/archiver"
 	"github.com/abonec/file_downloader/log"
+	"os"
+	"strconv"
+	"sync"
+	"net/http"
+	"fmt"
 )
+
+func getParallel() int {
+	env := os.Getenv("PARALLEL")
+	if env == "" {
+		return 2
+	}
+	size, err := strconv.Atoi(env)
+	if log.Error(err) {
+		os.Exit(1)
+	}
+	return size
+}
 
 func Download(inputQueue <-chan Input) <-chan archiver.Input {
 	ch := make(chan archiver.Input)
-	go func() {
-		for input := range inputQueue {
-			resp, err := http.Get(input.Url())
-			if log.Error(err) {
-				log.Warningf("Error while downloading %s", input.Url())
+	parallel := getParallel()
+	fmt.Printf("parallel: %d", parallel)
+	var wg sync.WaitGroup
+	for i := 0; i < parallel; i++ {
+		wg.Add(1)
+		go func() {
+			for input := range inputQueue {
+				resp, err := http.Get(input.Url())
+				if log.Error(err) {
+					log.Warningf("Error while downloading %s", input.Url())
+				}
+				ch <- &Result{resp.Body, input.Path()}
 			}
-			ch <- &Result{resp.Body, input.Path()}
-		}
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
 		close(ch)
 	}()
 	return ch
